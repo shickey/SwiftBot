b3c375a79ba96750e599364467985d2af1dba3f6
Sean Hickey <shickey@blakeschool.org>
2016-04-17 11:01:30 -0500

    Updated second map tutorial

--- SwiftBot.playground/Pages/Page 2 - turnLeft().xcplaygroundpage/Contents.swift
+++ SwiftBot.playground/Pages/Page 2 - turnLeft().xcplaygroundpage/Contents.swift
@@ -1,15 +1,30 @@
 //: [Previous](@previous)
 
-setup(1)
+setup(1) // Again, some necessary setup. Don't worry about it, but don't delete it either!
+
+/*:
+ 
+ # Turning Left
+ SwiftBot can also turn, but being the simple robot they are, they can only turn left. To tell SwiftBot to turn left, use the command:
+ 
+ ### `turnLeft()`
+ This is cause SwiftBot to turn left in place (i.e., they will stay on their current square)
+ 
+ ## Your Goal
+ Similar to before, tell SwiftBot to move to the end of the hallway and place a cookie there:
+ 
+ ![Turn Left and Drop a Cookie](turnleft_goal.png)
+ 
+ */
 
 instructions = {
     
-    goForward()
-    goForward()
-    turnLeft()
-    goForward()
-    goForward()
+    
     
 }
 
-//: [Next](@next)
+/*:
+ Click Next when you're ready to move on.
+ 
+ [Next](@next)
+ */
